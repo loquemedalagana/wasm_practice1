befc958604a103df3eb0155fe2a8267c32aa0f1b
Seongwon Kim <seongwon.kim@modelical.com>
2023-12-07 12:35:32 +0900

    - remove unused components

--- src/app/3d/page.tsx
+++ src/app/3d/page.tsx
@@ -1,22 +1,10 @@
-import dynamic from 'next/dynamic';
-
-import LoadingSpinner from '@/components/LoadingSpinner/LoadingSpinner';
-
-const CanvasProvider = dynamic(() => import('@/wgpu/CanvasContext'), {
-  loading: () => (
-    <section className="wrapper loading-wrapper">
-      <LoadingSpinner />
-    </section>
-  ),
-});
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CanvasProvider>
+      <>
         <h1>3D</h1>
         <h2>load obj file if not exist load default obj file</h2>
-      </CanvasProvider>
+      </>
     </main>
   );
 }
