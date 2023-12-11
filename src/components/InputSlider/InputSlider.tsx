b065044d210fe6ea6b549b18212ea1a3da6ccb81
Seongwon Kim <seongwon.kim@modelical.com>
2023-12-11 13:44:34 +0900

    - created vec3 input group

--- src/components/InputSlider/InputSlider.tsx
+++ src/components/InputSlider/InputSlider.tsx
@@ -1,14 +1,17 @@
 import { ChangeEventHandler } from 'react';
 
+import { V3 } from '@/util/types/math';
 import styles from '@/components/InputSlider/input_slider.module.css';
 
 interface InputSliderProps {
-  label?: 'x' | 'y' | 'z';
+  name?: V3;
   placeholder?: React.ReactNode;
   value: number;
   min: number;
   max: number;
   handleInput: ChangeEventHandler<HTMLInputElement>;
+  step?: number;
+  isVectorControl?: boolean;
 }
 
 const InputSlider: React.FC<InputSliderProps> = ({
@@ -16,20 +19,30 @@ const InputSlider: React.FC<InputSliderProps> = ({
   max,
   value,
   handleInput,
-  label,
+  name,
+  isVectorControl = false,
   placeholder,
+  step,
 }) => {
+  const boxStyle = isVectorControl
+    ? `${styles.range__slider} ${styles.vector_input}`
+    : `${styles.range__slider} ${styles.single_input}`;
+
   return (
-    <div className={styles.range__slider}>
+    <div className={boxStyle}>
       {placeholder}
-      <input
-        className={styles.sliderInput}
-        type="range"
-        min={min}
-        max={max}
-        defaultValue={value}
-        onChange={handleInput}
-      />
+      <div>
+        <input
+          name={name}
+          className={styles.sliderInput}
+          type="range"
+          min={min}
+          max={max}
+          defaultValue={value}
+          onChange={handleInput}
+          step={step}
+        />
+      </div>
     </div>
   );
 };
