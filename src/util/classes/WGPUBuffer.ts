3417772b2c752a6f0eaccd4b84ab8858d9acf818
Seongwon Kim <seongwon.kim@modelical.com>
2023-12-11 19:11:20 +0900

    - add index buffer in gpubuffer class

--- src/util/classes/WGPUBuffer.ts
+++ src/util/classes/WGPUBuffer.ts
@@ -2,12 +2,17 @@ class WGPUBuffer {
   buffers: GPUBuffer[];
   constructor(
     device: GPUDevice,
-    bufferDescriptors: Array<GPUBufferDescriptor & { data?: Float32Array }>,
+    bufferDescriptors: Array<
+      GPUBufferDescriptor & { data?: Float32Array; indices?: Uint32Array }
+    >,
   ) {
     this.buffers = bufferDescriptors.map((bufferDescriptor) => {
       const buffer = device.createBuffer(bufferDescriptor);
       if (bufferDescriptor.data) {
         new Float32Array(buffer.getMappedRange()).set(bufferDescriptor.data);
+        buffer.unmap();
+      } else if (bufferDescriptor.indices) {
+        new Uint32Array(buffer.getMappedRange()).set(bufferDescriptor.indices);
         buffer.unmap();
       }
       return buffer;
