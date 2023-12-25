a9ccb6d8f60994ac8329d2a4842afcacc8f15eff
Seongwon Kim <seongwon.kim@modelical.com>
2023-12-25 13:57:30 +0900

    - refactored classes

--- src/mashes/3d/Cube.ts
+++ src/mashes/3d/Cube.ts
@@ -1,6 +1,7 @@
 import { Vec3, vec3 } from 'wgpu-matrix';
+import Mesh from '@/mashes/3d/Mesh';
 
-class CubeMesh {
+class CubeMesh implements Mesh {
   vertices: Float32Array;
   positions: Float32Array;
   colors: Float32Array;
